02d5ba853de768748c5b47d60de6680a641b516d
Dominick Bruno <dbruno@unseen.is>
2019-10-30 22:16:18 -0400

    Add User and Fetch User List endpoints completed

--- index.js
+++ index.js
@@ -1,30 +1,61 @@
-// implement your API here
+// imports database and express
+const db = require('./data/db.js');
 const express = require('express');
 
+// create server (not best practices)
 const server = express();
+server.listen(4000, () => {
+  console.log('server listening on port 4000....');
+});
 
+// ensures json object is passed to server from req
+server.use(express.json());
+
+// Begin Endpoints
+
+// Block root path
 server.get('/', (req, res) => {
   res.status(403)('Forbidden');
 });
 
+// Add User
 server.post('/api/users', (req, res) => {
-    res.status(201).json(users);
-    });
+  const { name, bio } = req.body;
 
-server.get('/api/users', (req, res) => {
-  const hobbits = [
-    {
-      id: 1,
-      name: 'Samwise Gamgee',
-    },
-    {
-      id: 2,
-      name: 'Frodo Baggins',
-    },
-  ];
-
-  res.status(200).json(hobbits);
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: 'Please provide name and bio for the user.' });
+  } else {
+    Users.insert(req.body)
+      .then(user => {
+        res
+        .status(201)
+        .json(user);
+      })
+      .catch(() => {
+        res
+        .status(500)
+        .json({errorMessage: 'There was an error while saving the user to the database',
+        });
+      });
+  }
 });
+
+// Fetch User List
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => {
+      res
+      .status(200)
+      .json(users);
+      })
+    .catch(err => {
+      res
+      .status(500)
+      .json({success: false, errorMessage: 'The users information could not be retrieved.'});
+    });
+    });
 
 server.get('api/users/:id', (req, res) => {
     res.status(200).json(users.id);
@@ -36,6 +67,4 @@ server.delete('api/users/:id', (req, res) => {
 
 server.put('api/users/:id', (req, res) => {
     res.status(200);
-})
-
-server.listen(8000, () => console.log('API running on port 8000'));
+});
